e15328993abe5a287dc1d3f7a43e2031c6d1717b
Angrymouse <rykov.nikas@gmail.com>
2024-03-11 19:31:49 +0100

    return error handling to transfer.js

--- src/functions/transfer.js
+++ src/functions/transfer.js
@@ -1,8 +1,11 @@
 const Big = require('big.js');
 module.exports = function ({ input, caller }, changes) {
+    ContractAssert(typeof input.amount == "string" && !isNaN(amount), "Invalid amount provided")
+    ContractAssert(typeof input.to == "string", "No \"to\" specified");
 
     const to = input.to;
     const amount = Big(input.amount);
+    ContractAssert(amount.c == "0", "Cannot transfer fraction of minimal unit")
     
     const senderBalance = Big(KV.get(`balances.${caller}`) || "0")
     const receiverBalance = Big(KV.get(`balances.${to}`) || "0")
